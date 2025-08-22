5f0ba218df11deb3dd4fb8dc693a7800ab5941e4
Anh Duong Hoang <127716900+hoafnganh@users.noreply.github.com>
2025-08-22 21:35:38 +0700

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -2,30 +2,30 @@ const CONFIG = {
   // profile setting (required)
   profile: {
     name: "hoafnganh",
-    image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
+    image: "/avatar-1.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "cyber security learner",
-    bio: "I develop everything using node.",
+    bio: "Post and Telecommunications Institute of Technology.",
     email: "dhoanganh25705@gmail.com",
     github: "hoafnganh",
-    instagram: "",
+    instagram: "https://www.instagram.com/hoang.anh.2507/",
   },
   projects: [
     {
       name: `hoafnganh`,
-      href: "https://github.com/hoafnganh/hoafnganh",
+      href: "https://github.com/hoafnganh",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
+    title: "hoafnganh",
     description: "welcome to my blog!",
     scheme: "dark", // 'light' | 'dark' | 'system'
   },
 
   // CONFIG configration (required)
-  link: "https://morethan-log.vercel.app",
-  since: 2022, // If leave this empty, current year will be used.
-  lang: "en-US", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
+  link: "https://hoafnganh.vercel.app",
+  since: 2025, // If leave this empty, current year will be used.
+  lang: "vi-VN", // ['en-US', 'zh-CN', 'zh-HK', 'zh-TW', 'ja-JP', 'es-ES', 'ko-KR']
   ogImageGenerateURL: "https://og-image-korean.vercel.app", // The link to generate OG image, don't end with a slash
 
   // notion configuration (required)
@@ -68,7 +68,7 @@ const CONFIG = {
     },
   },
   isProd: process.env.VERCEL_ENV === "production", // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
-  revalidateTime: 21600 * 7, // revalidate time for [slug], index
+  revalidateTime: 21600 * 3, // revalidate time for [slug], index
 }
 
 module.exports = { CONFIG }
